f17fa55c2c0a5e6d20ce48690f8301c07cc5f17e
Tuminha <83960151+Tuminha@users.noreply.github.com>
2023-07-07 20:54:47 +0200

    Added a spinner comment Add a spinner to the process

--- app.py
+++ app.py
@@ -57,7 +57,7 @@ uploaded_files = st.file_uploader("Upload PDF Files", type=['pdf'], accept_multi
 
 if st.button('Rename PDFs'):
     if uploaded_files:
-        # Add a spinner while the app is renaming the pdfs
+        # Add a spinner while the app is renaming the pdfs so user sees the progress
         with st.spinner('Renaming PDFs...'):
             pass
         output_dir, renamed_count, same_name_count = rename_pdfs(uploaded_files)
